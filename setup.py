064f241ae47a17831ff4df11f7aad975f166bbc9
Fredy Hernandez <Fredy.1997dez@gmail.com>
2019-03-01 23:01:50 -0600

    se agrego el setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+
+	name = "hidepass",
+	version = "1.0",
+	description = "Muestra asteriscos al ingresar una contraseña por consola",
+	author = "Fredy Tecú",
+	author_email = "Fredy.1997dez@gmail.com",
+	packages = ["hidepass"]
+	)
